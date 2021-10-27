36f6ad830cf0b45b80807a4979bef23279d61fc8
MustoLini <johannes.danielsson@student.forsbergsskola.se>
2021-10-27 11:41:58 +0200

    PlayerChanges

--- Pumpkin-Madness/Assets/ID/ScriptID/Player.cs
+++ Pumpkin-Madness/Assets/ID/ScriptID/Player.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerID : MonoBehaviour
+public class Player : MonoBehaviour
 {
     public int maxHealth = 100;
     public int currentHealth;
