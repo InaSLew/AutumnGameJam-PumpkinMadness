cf553b4909416f652f13089296be01bfe84964ac
MustoLini <johannes.danielsson@student.forsbergsskola.se>
2021-10-27 12:51:23 +0200

    Light source and player script

--- Pumpkin-Madness/Assets/ID/ScriptID/Player.cs
+++ Pumpkin-Madness/Assets/ID/ScriptID/Player.cs
@@ -11,6 +11,7 @@ public class Player : MonoBehaviour
     private bool isInvincible = false; 
     [SerializeField] private float invincibilityDurationSeconds;
     [SerializeField] private float invincibilityDeltaTime;
+    [SerializeField] private GameObject model;
 
     public HealthBarID HealthBarID;
     // Start is called before the first frame update
