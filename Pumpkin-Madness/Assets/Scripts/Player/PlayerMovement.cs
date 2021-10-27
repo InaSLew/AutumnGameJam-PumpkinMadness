4bf75f5dcf14f17db9920d947c24aae2cd5f0737
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-10-27 12:01:15 +0200

    PlayerMovement basics nearly done

--- Pumpkin-Madness/Assets/Scripts/Player/PlayerMovement.cs
+++ Pumpkin-Madness/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public PlayerController playerController;
+    public float walkSpeed = 50f;
+    float moveLeftRight = 0f;
+    public bool jump = false;
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveLeftRight = Input.GetAxisRaw("Horizontal") * walkSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerController.Move(moveLeftRight *Time.deltaTime, false, jump);
+        jump = false;
     }
 }
