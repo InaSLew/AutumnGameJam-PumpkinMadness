379111bd7afed37d6e36fc210b46e802f5a695c2
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-10-27 10:24:01 +0200

    Small progress changes on enemy prefabs

--- Pumpkin-Madness/Assets/Scripts/Enemy/EnemyMovement.cs
+++ Pumpkin-Madness/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    private Transform playerTargetPosition;
+
+    public float MovementSpeed;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerTargetPosition = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(this.transform.position, playerTargetPosition.position, Time.deltaTime * MovementSpeed);
     }
 }
