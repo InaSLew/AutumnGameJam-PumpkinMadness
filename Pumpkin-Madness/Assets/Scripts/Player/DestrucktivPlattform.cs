90da6ad110a02968921886dabcaa470de6563d58
MustoLini <johannes.danielsson@student.forsbergsskola.se>
2021-10-28 16:25:05 +0200

    Update DestrucktivPlattform.cs

--- Pumpkin-Madness/Assets/Scripts/Player/DestrucktivPlattform.cs
+++ Pumpkin-Madness/Assets/Scripts/Player/DestrucktivPlattform.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class DestrucktivPlattform : MonoBehaviour
 {
     public float timer;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,20 +17,33 @@ public class DestrucktivPlattform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnCollisionStay2D(Collision2D other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player" )
         {
             // Thread.Sleep(4500);
             Invoke(nameof(Test), timer);
+            StartCoroutine(waitSecounds());
         }
+        
     }
 
     private void Test()
     {
         gameObject.SetActive(false);
+        
+    }
+
+    private void Test2()
+    {
+        gameObject.SetActive(true);
+    }
+
+    IEnumerator waitSecounds()
+    {
+        yield return new WaitForSeconds(4);
+        Invoke(nameof(Test2), timer);
     }
 }
