581e96a598a9c0d9c97c12cf3cdabb07ed44e0e4
Max Deurell Kent <max.deurell@gmail.com>
2021-10-28 08:39:34 +0200

    Camera now follows the player

---
+++ Pumpkin-Madness/Assets/Camera_Follow.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+    public Transform followTarget;
+    public Vector3 offset;
+  
+    private void moveCamera()
+    {
+        transform.position = new Vector3(followTarget.position.x + offset.x, followTarget.position.y + offset.y, followTarget.position.z + offset.z);
+    }
+    private void Update()
+    {
+        moveCamera();
+    }
+}
