888985255c04b94ee1f371eefd02a4b18c829db0
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-10-28 16:09:17 +0200

    Progress on weapon

--- Pumpkin-Madness/Assets/Scripts/Player/WeaponRotation.cs
+++ Pumpkin-Madness/Assets/Scripts/Player/WeaponRotation.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class WeaponRotation : MonoBehaviour
 {
+    private GameObject player;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.position = player.transform.position;
+        
+        
         Vector3 mousePos = Input.mousePosition;
         
         mousePos.z = 13;
