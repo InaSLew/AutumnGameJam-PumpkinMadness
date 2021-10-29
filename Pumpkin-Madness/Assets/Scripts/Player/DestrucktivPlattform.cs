aa62cbcda6ab8d294536565e2e7f3899c75521f8
MustoLini <johannes.danielsson@student.forsbergsskola.se>
2021-10-29 14:30:02 +0200

    Push

--- Pumpkin-Madness/Assets/Scripts/Player/DestrucktivPlattform.cs
+++ Pumpkin-Madness/Assets/Scripts/Player/DestrucktivPlattform.cs
@@ -1,23 +1,72 @@
+using System;
 using UnityEngine;
 
 public class DestrucktivPlattform : MonoBehaviour
 {
     private const float CoolDown = 4f;
+    private bool destroyPlatform;
+    
+    private float startTime =5;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Invoke(nameof(DisablePlatform) ,CoolDown);
+            destroyPlatform = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            destroyPlatform = false;
+            
+            
+        }
+        
+    }
 
     private void EnablePlatform()
     {
         gameObject.SetActive(true);
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-            Invoke(nameof(DisablePlatform), CoolDown);
-    }
+    // private void Update()
+    // {
+    //     if (currenttime==startTime)
+    //     {
+    //         Debug.Log("hello2");
+    //         DisablePlatform();
+    //     }
+    //     else if (currenttime>5)
+    //     {
+    //         currenttime = 0;
+    //     }
+    //     else if (currenttime<5)
+    //     {
+    //         Debug.Log("hello1");
+    //         currenttime+= 1 *Time.deltaTime;
+    //     }
+    // }
 
+    // private void OnCollisionEnter2D(Collision2D other)
+    // {
+    //     if (other.gameObject.CompareTag("Player"))
+    //         Invoke(nameof(DisablePlatform), CoolDown);
+    // }
+    //
     private void DisablePlatform()
     {
-        gameObject.SetActive(false);
-        Invoke(nameof(EnablePlatform), CoolDown);
+        if (destroyPlatform = true)
+        {
+            gameObject.SetActive(false);
+            Invoke(nameof(EnablePlatform), CoolDown);
+            destroyPlatform = false;
+            
+        }
+        
     }
 }
